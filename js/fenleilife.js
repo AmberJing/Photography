ecf84897ec84a6f1ea1e4222a8abbc82558e833a
琥珀 <807296988@qq.com>
2017-11-21 17:59:23 +0800

    添加product和about子页
    添加product和about子页

--- js/fenleilife.js
+++ js/fenleilife.js
@@ -5,12 +5,12 @@ $(function(){
 	var flag = false;	//当前是否有正在运行的定时器
 
 	// 加载更多
-	$('#loading .btn').click(function(){
+	$('#loading .loading-btn').click(function(){
 
 		// 没有正在执行的定时器
 		if(!flag){
 
-			$('#loading .btn .img').css('display','block');
+			$('#loading .loading-btn .loading-img').css('display','block');
 
 			flag = true;
 
@@ -25,8 +25,8 @@ $(function(){
 			for(var i = 0; i < times; i ++){
 
 				if(boxIndex >= fluid.length){
-					$('#loading .btn').text('没有内容了').css('background-color','rgba(0,0,0,0.4)');
-					$('#loading .btn').unbind('click');
+					$('#loading .loading-btn').text('没有内容了').css('background-color','rgba(0,0,0,0.4)');
+					$('#loading .loading-btn').unbind('click');
 					return;
 				}
 
@@ -34,11 +34,27 @@ $(function(){
 					temp = temp%4;
 
 				// 动态生成一个新的对象，并添加到指定位置 
-				var boxObj = '<div class="box"><a class="item_img"><img src="'+ fluid[boxIndex].item_img +'"  width="285px" alt=""><div class="dialog"><h2>'+ fluid[boxIndex].dialog_txt +'</h2><span>'+ fluid[boxIndex].dialog_span +'</span></div></a><div class="item_author"><img class="author_img" src="'+ fluid[boxIndex].head +'" width="50px" alt=""><h4>'+ fluid[boxIndex].name +'</h4><span>'+ fluid[boxIndex].state +'</span></div><div class="item_state" id="item_state" style="display:none;"><span>'+ fluid[boxIndex].author_state +'</span></div></div>';
+				var boxObj = '<div class="box">'
+							+'  <a class="item_img">'
+							+'	  <img src="'+ fluid[boxIndex].item_img +'"  width="285px" alt="">'
+							+'	  <div class="dialog">'
+							+'	     <h2>'+ fluid[boxIndex].dialog_txt +'</h2>'
+							+'	     <span>'+ fluid[boxIndex].dialog_span +'</span>'
+							+'	  </div>'
+							+'	</a>'
+							+'	<div class="item_author">'
+							+'	  <img class="author_img" src="'+ fluid[boxIndex].head +'" width="50px" alt="">'
+							+'	  <h4>'+ fluid[boxIndex].name +'</h4>'
+							+'	  <span>'+ fluid[boxIndex].state +'</span>'
+							+'	</div>'
+							+'	<div class="item_state" id="item_state" style="display:none;">'
+							+'	  <span>'+ fluid[boxIndex].author_state +'</span>'
+							+'	</div>'
+							+'</div>';
 				$('#show .show_icon').eq(temp).append(boxObj);
 				boxIndex ++; //box对象数组下标
 				temp ++;	//当前插入元素的父元素的下标
-				$('#loading .btn .img').css('display','none');
+				$('#loading .loading-btn .loading-img').css('display','none');
 			}
 			flag = false;
 		}
@@ -49,7 +65,7 @@ $(function(){
 	// 滚动监听事件
 	$(window).scroll(function(){
 		if( $(window).scrollTop() + $(window).height() >= $(document).height()){
-			$('#loading .btn').click();
+			$('#loading .loading-btn').click();
 		}
 			
 	});
